fa73a2af0ae893e728f7097a4f80ec884442af60
mindedge26 <mindedge26@MEDGE164>
2022-07-20 15:14:33 +0530

    Add project files.

---
+++ WebApplication1/Models/Employe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Employe
+    {
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNo { get; set; }
+    }
+}
